f28e7d2a0419c6925bd432ea68a1d530725b1b60
Romit Karmakar <romitkarmakar@gmail.com>
2020-01-09 18:45:15 +0000

    Bugs fixed

--- lib/7-up-7-down/layout.dart
+++ lib/7-up-7-down/layout.dart
@@ -47,15 +47,11 @@ class _UpDownGameState extends State<UpDownGame> {
     data: {"email": "romitkarmakar@gmail.com"});
 
     choice=response.data["result"];
-<<<<<<< HEAD
-=======
-    coins_left=response.data["coins"];
->>>>>>> 18c7676697f760ecca6ca36ccfd7b25bf1a79faa
+
   } catch (e) {
     print(e);
   }
 }
-<<<<<<< HEAD
 void submit() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   try {
@@ -63,13 +59,6 @@ void submit() async {
     data: {"email": "romitkarmakar@gmail.com", "status": result});
     coins_left=response.data["coins"];
     await prefs.setInt('coins', coins_left);
-=======
-void wish2() async {
-  try {
-    Response response = await Dio().post("https://aavishkargames.herokuapp.com/sevenup/create", 
-    data: {"email": "romitkarmakar@gmail.com"});
-    coins_left=response.data["coins"];
->>>>>>> 18c7676697f760ecca6ca36ccfd7b25bf1a79faa
   } catch (e) {
     print(e);
   }
